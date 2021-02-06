135df8c29debef7120f76d873f4e8da484091b59
mazurenkooleksandr <kakchtoinfo@ukr.net>
2021-02-06 10:53:03 +0200

    added function changeTable

--- popup.js
+++ popup.js
@@ -16,26 +16,34 @@ window.onclick = function (event) {
    }
 }
 
-let tableEvent = {
-   'participants': ['1', '2', '3', '4', '5', '6'],
-   'day': ['1', '2', '3', '4', '5'],
-   'time': ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-}
+function changeTable() {
 
+   let e = document.getElementById("col");
+   let cl = e.options[e.selectedIndex].value;
+   let r = document.getElementById("row");
+   let rw = r.options[r.selectedIndex].value;
+   let tabs = document.getElementById("tbl");
+   let trs = tabs.getElementsByTagName("tr")[0];
+   let text = document.getElementById('tblText').value;
 
-//let tds = mainTbl.getElementsByTagName("td");
-
-
-function changeTable () {
-   let text = document.getElementById("tblText").value;
-   let cells = document.getElementsByTagName('tr') 
-   var selectTime = document.getElementById("row");
-   var valueTime = selectTime.value;
-   /*var select = document.getElementById("col");
-        var value = select.options[select.selectedIndex].value;
-        alert(value);*/
-        text.innerHTML = cells;
-
-   modal.style.display = "none";
    
+
+   
+
+   for (let i = 0, row; row = tabs.rows[i]; i++) {
+       for (let j = 0, col; col = row.cells[j]; j++) {
+               if (trs.cells[cl].id !== 'unique_id') {
+                   trs = tabs.getElementsByTagName("tr")[rw];
+                   trs.cells[cl].innerHTML = text;
+                   trs.cells[cl].id = "unique_id";
+                   trs.cells[cl].style.backgroundColor = "#d3f5b4";  
+
+                   
+                  }else if (trs.cells[cl].id === 'unique_id') {
+                     console.log('неможливо записати');
+                  } 
+                  modal.style.display = "none";
+               
+         }
+   }
 }
